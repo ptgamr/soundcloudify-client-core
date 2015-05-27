d5f05ea35434216100e4e814c979a3ab646531ad
Anh Trinh <anh@catalyst.net.nz>
2015-05-27 13:32:47 +1200

    init core module

---
+++ directives/loadingDirective.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    function loadingDirective() {
+
+        var initParams = function($scope) {
+            $scope.ver = $scope.variation || 4;
+        };
+
+        return {
+            restrict: 'E',
+            scope: {
+                'variation': '@'
+            },
+            templateUrl: 'scripts/views/loadingDirective.html',
+            link : function($scope) {
+                initParams($scope);
+            }
+        };
+    };
+
+    var commonModule = angular.module('soundcloudify.core');
+    commonModule.directive('loading', loadingDirective);
+}());
